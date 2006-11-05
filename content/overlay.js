8a364b0cdeae4ec911313cae384d0f4111e450a2
GraemeWorthy <GraemeWorthy@c384ee3d-7b21-0410-8046-113b1a2aefce>
2006-11-05 11:24:10 +0000

    added: a selector pulldown also: a success page

--- content/overlay.js
+++ content/overlay.js
@@ -1,13 +1,14 @@
-var EggOn = {
+var eggon = {
   onLoad: function() {
     // initialization code
     this.initialized = true;
   },
 
   onMenuItemCommand: function() {
-    window.open("chrome://eggon/content/eggon.xul", "", "chrome");
+   // window.openDialog("chrome://eggon/content/eggon.xul", "", "chrome, centerscreen, modal");
+    window.openDialog("chrome://eggon/content/success.xul", "", "chrome, centerscreen, modal");
   }
 };
 
-window.addEventListener("load", function(e) { EggOn.onLoad(e); }, false); 
+window.addEventListener("load", function(e) { eggon.onLoad(e); }, false); 
 
